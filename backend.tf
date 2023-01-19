f320740ba8839738e08bfb0eef4f1eadf6e1d2e5
Maheshmolli <97662716+Maheshmolli@users.noreply.github.com>
2023-01-19 11:39:06 +0530

    Update backend.tf
    changed region details

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "hastag-terraform-project-maahiInfo"
     key = "main"
-    region = "ap-northeast-1c"
+    region = "ap-northeast-1"
     dynamodb_table = "hashtagTerraformStatelock"
   }
 }
