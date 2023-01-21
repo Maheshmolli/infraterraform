ceecefa49c9d2e1a2c2b8b33efe0484ad6a3c52f
Maheshmolli <97662716+Maheshmolli@users.noreply.github.com>
2023-01-21 15:34:17 +0530

    Update backend.tf
    s3 name updated

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "hastag-terraform-project-maahiInfo"
+    bucket = "hastag-terraform-project-maahi"
     key = "main"
     region = "ap-northeast-1"
     dynamodb_table = "hashtagTerraformStatelock"
